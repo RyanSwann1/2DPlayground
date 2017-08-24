4776b5059e85a186132c0a82e4c86278ff4f13a3
Ryan Swann <RyanSwann1992@gmail.com>
2017-08-24 11:59:25 +0100

    Added game window.

--- 2DPlayground/src/main.cpp
+++ 2DPlayground/src/main.cpp
@@ -1,11 +1,30 @@
 
 #include <iostream>
 #include <string>
+#include <SFML\Graphics.hpp>
+#include <SFML\Window.hpp>
 
 int main()
 {
 	std::cout << "Hello World.\n";
 
+	sf::Window window(sf::VideoMode(640, 480), "SFML_Window");
+	while (window.isOpen())
+	{
+		sf::Event sfmlEvent;
+		while (window.pollEvent(sfmlEvent))
+		{
+			switch (sfmlEvent.type)
+			{
+			case sf::Event::Closed:
+			{
+				window.close();
+				break;
+			}
+			}
+		}
+
+	}
 
 	char c;
 	std::cin >> c;
